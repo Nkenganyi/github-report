d6d36bf0775991757810cbb5f93c6e440cd6fabd
Nkenganyi <ntungnkenganyi54@gmail.com>
2021-02-27 12:01:32 +0200

    github report project

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import { GithubApiService } from "./GithubApiService";
+import * as _ from 'lodash';
+import { Repo } from "./Repo";
+import { User } from "./User";
+
+let svc  = new GithubApiService();
+
+if(process.argv.length < 3){
+    console.log('Please pass the username as an argument')
+}else{
+    let username = process.argv[2];
+
+svc.getUserInfo(username, (user: User) => {
+    svc.getRepos(username, (repos: Repo[]) => {
+        let sortRepos = _.sortBy(repos, [(repo: Repo) => repo.size * -1]);
+        let filteredRepos = _.take(sortRepos, 5);
+        user.repos = filteredRepos;
+        console.log(user);
+    });
+
+});
+}
+
+
+
+
